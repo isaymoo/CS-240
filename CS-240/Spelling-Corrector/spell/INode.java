97a4184d09e43593f9ebbdd5bb4207b7797ed964
isaymoo <59543026+isaymoo@users.noreply.github.com>
2020-04-30 15:12:57 -0600

    Add files via upload

---
+++ CS-240/Spelling-Corrector/spell/INode.java
@@ -0,0 +1,14 @@
+package spell;
+
+/**
+ * Your trie node class should implement the INode interface
+ */
+public interface INode {
+
+    /**
+     * Returns the frequency count for the word represented by the node
+     *
+     * @return The frequency count for the word represented by the node
+     */
+    public int getValue();
+}
